a6dbcc89a69b90bb673e5cd33f22666146729d64
Xavier RODET <xavier.rodet@gmail.com>
2019-03-04 09:09:42 +0100

    remove softExit option because we can't do hardExit

--- src/Strategy/Timer.php
+++ src/Strategy/Timer.php
@@ -7,32 +7,19 @@ namespace Snailweb\Utils\Strategy;
 class Timer extends AbstractStrategy
 {
     private $maxTime;
-    private $softExit;
     private $startTime;
 
-    public function __construct(int $maxTime, $softExit = true)
+    public function __construct(int $maxTime)
     {
         $this->maxTime = $maxTime;
-        $this->softExit = $softExit;
         parent::__construct();
     }
 
     protected function buildCondition() : \Closure
     {
         return function() {
-
             $runTime = time() - $this->startTime;
-            if($runTime < $this->maxTime) {
-                return true;
-            }
-            else {
-
-                if ($this->softExit) {
-                    return false;
-                } else {
-                    exit(0);
-                }
-            }
+            return ($runTime < $this->maxTime);
         };
     }
 
